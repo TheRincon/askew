ffdb540530b2ebc5e49e14b6230192a52f6f307f
TheRincon <rinconrex@gmail.com>
2020-04-10 22:12:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Perspective Transformation along specific axes
+
+This is a rewrite of [the original repo](https://github.com/eborboihuc/rotate_3d)
+
+Why a clone and not a fork? I think the original repo is great, and it is a really cool project that I use all the time. That being said, it has too many comments, requires manual changing of parameters everytime and does not support python3. I added proportional rotation for all three axes, and an option to make a gif from the tool, if imagemagick is installed. This was a fairly small change of course, but I also plan to add more image resizing based on the change in perspective (with the original you must calculate the ideal height and width yourself and pass it into the program as an awkward tuple argument).
+
+I would try to fork, and make a PR but it does not look like it is supported.
+
+Finally I added the option to make a GIF with a flag if imagemagick is installed.
+
+## Author
+
+Hou-Ning Hu / [@eborboihuc](https://eborboihuc.github.io/)
+
+## Prerequisites
+
+Contrary to what it says on [the original repo](https://github.com/eborboihuc/rotate_3d)
+
+This version is made for:
+- Python3 with numpy
+- OpenCV
+- Imagemagick* if you wish to make a GIF
+
+It should be able to run on any unix system with Python3, and does not require a specific version of OpenCV
+
+## Usage
+
+```bash
+askew.py [-i image_path] [-o outout] [--mode] [--theta] [--phi] [--gamma] [--length] [--width] [--dx] [--dy] [--dz]
+```
+
+Example of rotating an image along yz-axis from 0 to 360 degree with a 5 pixel shift in +X direction and making a GIF:
+
+```bash
+python3 askew.py -i images/example.jpg -o output --mode multi --phi 360 --gamma 360 --dx 5 -j
+```
+
+Single image:
+
+```bash
+python3 askew.py -i images/example.jpg --mode single  --theta 120 --gamma 120  --dz 10
+```
+
+## Animation
+
+![](example/output.gif)
+Rotate along YZ axis and translate 5 pixel along X axis
+
+![](example/output.jpg)
+Rotate along XZ axis
+
+## Parameters:
+
+```python
+it = ImageTransformer(img_path, height=height, width=width)
+it.rotate_along_axis(theta=0, phi=0, gamma=0, dx=0, dy=0, dz=0):
+```
+- image_path   : the path of image that you want rotated
+- output       : name of the output image/gif
+- height       : the height of output image, default is original size.
+- width        : the width of output image, default is original size.
+- theta        : the rotation around the x axis
+- phi          : the rotation around the y axis
+- gamma        : the rotation around the z axis (basically a 2D rotation)
+- dx           : translation along the x axis
+- dy           : translation along the y axis
+- dz           : translation along the z axis (distance to the image)
+
+## Acknowledgments
+
+Code ported and modified from [jepson](http://jepsonsblog.blogspot.tw/2012/11/rotation-in-3d-using-opencvs.html) and [stackoverflow](http://stackoverflow.com/questions/17087446/how-to-calculate-perspective-transform-for-opencv-from-rotation-angles). Thanks for their excellent work!
+
---
+++ askew.py
@@ -0,0 +1,71 @@
+from image_transformer import ImageTransformer
+from util import save_image
+import sys
+import os
+import subprocess
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(
+    description='Python3 Image Transformer',
+    usage="askew.py [-i image_path] [-o outout] [--mode] [--theta] [--phi] [--gamma] [--height] [--width] [--dx] [--dy] [--dz]")
+parser.add_argument('-i', dest="image_path", action="store", metavar='image_path',
+                    type=str, help="input image path", required=True)
+parser.add_argument('--mode', dest="mode", metavar='mode',
+                    type=str, help="what command will generate (single image type or gif)", required=True)
+parser.add_argument('-o', dest="output", metavar='output', action="store", type=str,
+                    help='output dir or single image', default='output')
+parser.add_argument('--theta', metavar='theta', type=int,
+                    help='rotation around the x-axis', default=0)
+parser.add_argument('--phi', metavar='phi', type=int,
+                    help='rotation around the y-axis', default=0)
+parser.add_argument('--gamma', metavar='gamma', type=int,
+                    help='rotation around the z-axis ("normal" rotation)', default=0)
+parser.add_argument('--height', metavar='height', type=int,
+                    help='set height of output image', default=None)
+parser.add_argument('--width', metavar='width', type=int,
+                    help='set width of the output image', default=None)
+parser.add_argument('--dx', metavar='dx', type=int,
+                    help='pixel shift in the x direction', default=0)
+parser.add_argument('--dy', metavar='dy', type=int,
+                    help='pixel shift in the y direction', default=0)
+parser.add_argument('--dz', metavar='dz', type=int,
+                    help='pixel shift in the z direction (image distance)', default=0)
+parser.add_argument('-j', help='make GIF of output', action='store_true')
+
+options = parser.parse_args()
+
+if options.image_path is None:
+    raise ValueError('Image not found, check image path')
+
+filename, file_extension = os.path.splitext(options.image_path)
+
+it = ImageTransformer(options.image_path, height=options.height, width=options.width)
+
+if not os.path.isdir('output'):
+    os.mkdir('output')
+
+if options.mode == 'single':
+    rotated_img = it.rotate_along_axis(theta=options.theta, phi=options.phi, gamma=options.gamma, dx=options.dx, dy=options.dy, dz=options.dz)
+    save_image(f'example/{options.output}{file_extension}', rotated_img)
+elif options.mode == 'multi':
+    rot_array = [rot for rot in [options.theta, options.phi, options.gamma, options.dx, options.dy, options.dz] if rot is not 0]
+    if len(rot_array) == 0:
+        raise ValueError('No rotations were set, please use command line flags -p, -t, -g, dx, dy, or dz and set at least one to an int')
+    rot_range = max(rot_array) if max(rot_array) > 0 else 360
+    for ang in range(0, rot_range):
+        rotated_img = it.rotate_along_axis(
+            theta=np.linspace(0,options.theta,rot_range, dtype = int)[ang],
+            phi=np.linspace(0,options.phi,rot_range, dtype = int)[ang],
+            gamma=np.linspace(0,options.gamma,rot_range, dtype = int)[ang],
+            dx=np.linspace(0,options.dx,rot_range, dtype = int)[ang],
+            dy=np.linspace(0,options.dy,rot_range, dtype = int)[ang],
+            dz=np.linspace(0,options.dz,rot_range, dtype = int)[ang]
+        )
+        save_image('output/{}-{}.jpg'.format(options.output, str(ang)), rotated_img)
+    if options.j:
+        subprocess.run(["convert", f"output/{options.output}-%d{file_extension}[0-{rot_range - 1}]", f"example/{options.output}.gif"])
+else:
+    raise ValeuError('Mode not recognized')
+
+
